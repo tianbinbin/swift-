e2a93d8498b018f2b655d60fd7913512ef8d9424
田彬彬 <tbb@tianbinbindeMac-mini.local>
2017-04-22 23:16:27 +0800

    创建项目

---
+++ swift-闭包/swift-闭包/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-闭包
+//
+//  Created by 田彬彬 on 2017/4/22.
+//  Copyright © 2017年 田彬彬. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
