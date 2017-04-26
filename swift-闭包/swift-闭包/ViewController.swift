b536473585e13c1617b3aa5d0c2a5f6c0529aa20
田彬彬 <tbb@tianbinbindeMac-mini.local>
2017-04-26 12:45:14 +0800

    介绍闭包的几种写法。包括一个委随闭包。菜鸟的话 就别了。还时一步一步来吧

--- swift-闭包/swift-闭包/ViewController.swift
+++ swift-闭包/swift-闭包/ViewController.swift
@@ -24,7 +24,8 @@ class ViewController: UIViewController {
         
         print("点击了屏幕------")
         
-        
+        // 解决方案一
+        /*
         weak var weakselft:ViewController? = self
         
         // 闭包相当于 oc 的 block
@@ -35,9 +36,40 @@ class ViewController: UIViewController {
             //　这里self。 一半可以省略到 
             // 1. 如果改方中 局部变量于成员属性之间有歧义 （名称相同）
             // 2. 在闭包中使用成员属性
+            // 这里要注意 是否会造成循环引用
             weakselft?.view.backgroundColor = UIColor.red
             
         })
+        */
+        
+        
+        
+        // 第二种解决方法
+        
+        httptool?.LoadData({[weak self](jsonData:String) in
+            
+            self?.view.backgroundColor = UIColor.blue
+            
+        })
+ 
+        /*
+        // 第三种不建议用 会造成野指针
+        httptool?.LoadData({ [unowned self ] (jsonData:String) in
+            
+             self.view.backgroundColor = UIColor.blue
+            
+        })
+        */
+        
+        
+        // 尾随闭包的概念刚开始不建议
+        // 如果在函数中 闭包是最后一个参数 那么可以写成尾随闭包
+        // 如果时唯一的参数 也可以将前面的()省略掉
+        httptool?.LoadData(){[weak self](jsonData:String) in
+            
+            self?.view.backgroundColor = UIColor.blue
+        }
+    
     }
     
     deinit {
@@ -45,8 +77,6 @@ class ViewController: UIViewController {
         print("该对象释放了")
         
     }
-
-
 
 }
 
